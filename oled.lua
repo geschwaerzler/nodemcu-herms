fe6889196f4139a392b324b48daef375ada596b4
Gerhard Schwaerzler <gerhard@schwaerzler.at>
2018-03-11 15:38:08 +0100

    circular MT meter design.

--- oled.lua
+++ oled.lua
@@ -14,6 +14,18 @@ local hltSet = 0
 local hltActual = 22.3
 local coil = 24.5
 
+-- compiled fonts
+local helvetica8 = u8g.font_helvR08r
+local helvetica10 = u8g.font_helvR10r
+local helvetica10b = u8g.font_helvB10r
+local helvetica12b = u8g.font_helvB12r
+local helvetica14b = u8g.font_helvB14r
+local newcentury8 = u8g.font_ncenR08r
+local newcentury10 = u8g.font_ncenR10r
+local newcentury10b = u8g.font_ncenB10r
+local newcentury12b = u8g.font_ncenB12r
+local newcentury14b = u8g.font_ncenB14r
+
 -- display functions
 local nextDrawFunc = nil            -- next screen to be drawn
 local drawQueueEmpty = true
@@ -39,7 +51,7 @@ function display(func)
         if disp:nextPage() then
             node.task.post(node.task.MEDIUM_PRIORITY, drawPage)
         else
---            print('draw loop finished after:', math.floor( (tmr.now()-startTime)/1000 ))
+            print('draw loop finished after:', math.floor( (tmr.now()-startTime)/1000 ))
             if nextDrawFunc then
                 func = nextDrawFunc
                 nextDrawFunc = nil
@@ -73,23 +85,41 @@ function drawCenteredStr(x, y, w, str)
     disp:drawStr(x, y, str)
 end
 
-function drawPanel(x, y, w, h, title)
-    disp:setFont(u8g.font_6x10)
-    disp:setDefaultForegroundColor()
-    disp:drawFrame(x, y+3, w, h-4)
-    drawCenteredStr(x, y+7, w, title)
+function drawCenteredTemp(x, y, w, temp, fontInt, fontFract)
+    local intStr = string.format('%d', temp)
+    local fract = math.abs(temp)
+    fract = fract - math.floor(fract)
+    local fractStr = string.format('.%d', fract*10)
+    disp:setFont(fontInt)
+    local strW = disp:getStrWidth(intStr)
+    disp:setFont(fontFract)
+    strW = strW + disp:getStrWidth(fractStr)
+    x = math.floor(x + (w-strW)/2)
+    disp:setFont(fontInt)
+    x = x + disp:drawStr(x, y, intStr)    
+    disp:setFont(fontFract)
+    disp:drawStr(x, y, fractStr)
+end
+
+function drawPanel(x, y, w, h, title, font)
+    disp:setFont(u8g.font_chikita)
+    local ascent = disp:getFontAscent()
+    local mid = math.floor(ascent/2)
+    disp:drawFrame(x, y+mid, w, h-mid)
+    drawCenteredStr(x, y+ascent, w, title)
+    return (ascent + h) / 2 -- returns dy of the middle
 end
 
 function drawTemp(x, y, title, temp)
-    drawPanel(x, y, 31, 28, title)
-    local tempStr = string.format('%.1f', temp)
-    disp:drawStr(centerStr(x, 31, tempStr), y+20, tempStr)
+    local dy = drawPanel(x, y, 31, 31, title, u8g.font_chikita)
+    dy = math.ceil(dy + disp:getFontAscent()/2)
+    drawCenteredTemp(x, y+dy, 31, temp, newcentury10b, newcentury8)
 end
 
 function drawMT(x, y, r, t, dt)
     -- circular scale
     disp:drawCircle(x+r, y, r-2, u8g.DRAW_UPPER_LEFT)
-    disp:drawDisc(x+r, y, r-1, u8g.DRAW_UPPER_RIGHT)
+    disp:drawDisc(x+r, y, r-2, u8g.DRAW_UPPER_RIGHT)
     disp:setDefaultBackgroundColor()
     disp:drawDisc(x+r, y, r-4, u8g.DRAW_UPPER_RIGHT)
     disp:setDefaultForegroundColor()
@@ -111,37 +141,52 @@ function drawMT(x, y, r, t, dt)
     local angle = math.pi * dt / 4
     local angleSqr = angle * angle
     local angleBy3 = angleSqr*angle
-    local sin = angle - angleBy3 / 6 + angleBy3*angleSqr / 120
-    local cos = 1 - angleSqr/2 + angleSqr*angleSqr/24 - angleBy3*angleBy3/720
-
+    local angleBy4 = angleSqr*angleSqr
+    local sin = angle - angleBy3/6 + angleBy3*angleSqr/120 - angleBy4*angleBy3/5040
+    local cos = 1 - angleSqr/2 + angleBy4/24 - angleBy3*angleBy3/720
+    
     local dx, dy = math.floor(sin*r + 0.5), math.floor(cos*r + 0.5)
---    print('needle: ', x+r, y, x+r+dx, y-dy)
     disp:drawLine(x+r, y, x+r+dx, y-dy)
+    disp:setDefaultBackgroundColor()
+    disp:drawDisc(x+r, y, r-12, u8g.DRAW_UPPER_LEFT)
+    disp:drawDisc(x+r, y, r-12, u8g.DRAW_UPPER_RIGHT)
+    disp:setDefaultForegroundColor()
 
     -- draw temp value
-    drawCenteredStr(x, y, 2*r, string.format('%.1f', t))
+    drawCenteredTemp(x, y, 2*r, t, newcentury14b, newcentury10)
+end
+
+function drawMem(x, y, used, free)
+    local blockW = 16
+    local blockCount = 128 / blockW
+
+    disp:setFont(u8g.font_chikita)
+    local total = math.ceil((used+free) / 1024 / blockCount) * blockCount * 1024
+    local h = disp:getFontAscent()
+    disp:drawFrame(x, y-h, 128, h)
+    for tick = blockW,128-blockW,blockW do
+        disp:drawVLine(tick, y-h, h)
+    end
+    local usedW = math.floor( (total-free)/total*128 + 0.5 )
+    disp:drawBox(x, y-h, usedW, h)
+    disp:setDefaultBackgroundColor()
+    for tick = blockW,usedW,blockW do
+        disp:drawVLine(tick, y-h+1, h-2)
+    end
+    local blockStr = string.format('%01dK', total/1024/blockCount)
+    disp:drawStr(centerStr(x,blockW,blockStr), y, blockStr)
+    disp:setDefaultForegroundColor()
 end
 
 function drawHermsState(hltSet, hltActual, coil)
+    local memUsed = collectgarbage('count')*1024
+    local memFree = node.heap()
     return function ()
         drawTemp(0, 0, 'HLT', hltActual)
-        drawTemp(0, 28, 'Coil', coil)
-        drawTemp(32, 0, 'set', hltSet)
-        drawMT(64, 32, 32, 45.32, hltSet/10)
-        
-        disp:setFont(u8g.font_6x10)
-        disp:setDefaultForegroundColor()
-        local offset = disp:drawStr(0, 64, 'Mem')
-        local totalMem = 50000
-        local barHeight = 8
-        local barWidth = 128 - offset
-        -- draw a box for memory used
-        local w = math.floor(collectgarbage('count')*1024*barWidth / totalMem)
-        disp:drawBox(offset, 64-barHeight, w, barHeight)
-        offset = offset + w
-        -- draw a frame for available memory
-        w = math.floor(node.heap()*barWidth / totalMem)
-        disp:drawFrame(offset, 64-barHeight, w, barHeight)
+        drawTemp(96, 0, 'Coil', coil)
+--        drawTemp(32, 0, 'set', hltSet)
+        drawMT(24, 54, 40, 45.32, hltSet/10)
+        drawMem(0, 64, memUsed, memFree)
     end
 end
 
